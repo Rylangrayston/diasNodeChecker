94298e011106a4ddba2eee77371cb9a750d1dd95
rylangrayston <rylangrayston@gmail.com>
2017-05-07 02:27:59 -0400

    made it work and added speach

--- nodeChecker.py
+++ nodeChecker.py
@@ -1,12 +1,27 @@
 import os
-pathToNodeDirectories = "/home/rylangrayston/Desktop/nodeChecker/nodeFiles/"
+import pyttsx
+import time
+engine = pyttsx.init()
 
-nodesInSpreadSheet = []
-with open( "/home/rylangrayston/Desktop/nodeChecker/nodes.csv") as f:
-	for line in f.readlines():
-		print line.strip()
-		nodesInSpreadSheet.append(line.strip())
+while True:
 
-nodeDirectories = os.listDir(pathToNodeDirectories)
+	pathToNodeDirectories = "/home/rylangrayston/git/diasNodeChecker/testNodeData/"
 
+	nodesInSpreadSheet = []
+	with open("/home/rylangrayston/git/diasNodeChecker/test.csv") as f:
+		for line in f.readlines():
+			#print line.strip()
+			nodesInSpreadSheet.append(line.strip())
+
+	nodeDirectories = os.listdir(pathToNodeDirectories)
+	#print nodeDirectories
+
+	for node in nodesInSpreadSheet:
+		if node not in nodeDirectories:
+			print node
+			engine.say(node[0])
+			engine.runAndWait()
+			engine.say(node[1])
+			engine.runAndWait()
+			time.sleep(2)
 
